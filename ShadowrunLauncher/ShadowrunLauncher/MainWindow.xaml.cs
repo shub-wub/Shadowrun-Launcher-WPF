3bc121cd61f225bc171f3577c7ee98dbd83785da
FiveDollar Bagel <spoofer10@live.com>
2024-05-10 02:00:26 -0400

    -Added sound effects

--- ShadowrunLauncher/ShadowrunLauncher/MainWindow.xaml.cs
+++ ShadowrunLauncher/ShadowrunLauncher/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ShadowrunLauncher.Logic;
 using System.Diagnostics;
+using System.IO;
 
 namespace ShadowrunLauncher
 {
@@ -16,13 +17,23 @@ namespace ShadowrunLauncher
         private Point startPoint;
         private InstallLogic _installLogic;
 
+        // Dictionary to hold MediaPlayer instances for different sound channels
+        private Dictionary<string, MediaPlayer> soundChannels = new Dictionary<string, MediaPlayer>();
+
+        //// Adjust this value to set the background music volume (0.0 to 1.0)
+        private double backgroundMusicVolume = 0.1;
+        private double buttonClickVolume = 0.4;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeSoundChannels();
             StartGlowAnimation();
             MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
             MouseMove += MainWindow_MouseMove;
             MouseLeftButtonUp += MainWindow_MouseLeftButtonUp;
+
+            PlayBackgroundMusic("Audio/backgroundAmbience.wav");
 
             // Buttons
             playButton.PreviewMouseLeftButtonDown += PlayButton_PreviewMouseLeftButtonDown;
@@ -62,6 +73,72 @@ namespace ShadowrunLauncher
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             _installLogic.CheckForUpdates();
+        }
+
+        private void InitializeSoundChannels()
+        {
+            // Add sound channels for different types of sounds
+            AddSoundChannel("backgroundMusic");
+            AddSoundChannel("buttonClick");
+            AddSoundChannel("buttonHover");
+        }
+
+        private void AddSoundChannel(string channelName)
+        {
+            // Create a new MediaPlayer instance for the channel
+            soundChannels[channelName] = new MediaPlayer();
+        }
+
+        private void PlaySound(string relativePath, string channelName, double volume)
+        {
+            // Get the absolute path of the audio file
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string absolutePath = Path.Combine(basePath, relativePath);
+
+            // Check if the file exists
+            if (File.Exists(absolutePath))
+            {
+                // Get the MediaPlayer instance for the specified channel
+                MediaPlayer mediaPlayer = soundChannels[channelName];
+
+                // Load the sound file
+                mediaPlayer.Open(new Uri(absolutePath));
+
+                // Set the volume
+                mediaPlayer.Volume = volume;
+
+                // Play the sound
+                mediaPlayer.Play();
+            }
+            else
+            {
+                MessageBox.Show("Sound file not found.");
+            }
+        }
+
+        private void PlayBackgroundMusic(string relativePath)
+        {
+            // Use the class-level variable directly to set the volume
+            PlaySound(relativePath, "backgroundMusic", backgroundMusicVolume);
+        }
+
+        private void SetBackgroundMusicVolume(double volume)
+        {
+            // Check if the background music player exists and has a source set
+            if (soundChannels.ContainsKey("backgroundMusic") && soundChannels["backgroundMusic"].Source != null)
+            {
+                // Ensure the volume is within the valid range (0.0 to 1.0)
+                if (volume < 0.0)
+                    volume = 0.0;
+                else if (volume > 1.0)
+                    volume = 1.0;
+
+                // Set the volume of the background music player
+                soundChannels["backgroundMusic"].Volume = volume;
+
+                // Update the backgroundMusicVolume property
+                backgroundMusicVolume = volume;
+            }
         }
 
         private void StartGlowAnimation()
@@ -104,6 +181,9 @@ namespace ShadowrunLauncher
 
         private void PlayButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_clicked.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -143,6 +223,9 @@ namespace ShadowrunLauncher
 
         private void PlayButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_highlight.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -159,6 +242,9 @@ namespace ShadowrunLauncher
 
         private void DiscordButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_clicked.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -208,6 +294,9 @@ namespace ShadowrunLauncher
 
         private void DiscordButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_highlight.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -224,6 +313,9 @@ namespace ShadowrunLauncher
 
         private void WebsiteButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_clicked.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -273,6 +365,9 @@ namespace ShadowrunLauncher
 
         private void WebsiteButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             Uri uri = new Uri("pack://application:,,,/Images/button_generic_highlight.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -289,6 +384,9 @@ namespace ShadowrunLauncher
 
         private void CloseButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             Uri uri = new Uri("pack://application:,,,/Images/close_button_clicked.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -328,6 +426,9 @@ namespace ShadowrunLauncher
 
         private void CloseButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             Uri uri = new Uri("pack://application:,,,/Images/close_button_highlight.png");
             BitmapImage bitmap = new BitmapImage(uri);
@@ -344,6 +445,9 @@ namespace ShadowrunLauncher
 
         private void QuestionButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             questionImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/question_button_clicked.png"));
 
@@ -389,6 +493,9 @@ namespace ShadowrunLauncher
 
         private void QuestionButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             questionImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/question_button_highlight.png"));
         }
@@ -401,6 +508,9 @@ namespace ShadowrunLauncher
 
         private void MinimizeButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Play Sound On Click
+            PlaySound("Audio/buttonClick.wav", "buttonClick", buttonClickVolume);
+
             // Change the image source to the clicked version
             minimizeImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/minimize_button_clicked.png"));
 
@@ -436,6 +546,9 @@ namespace ShadowrunLauncher
 
         private void MinimizeButton_MouseEnter(object sender, MouseEventArgs e)
         {
+            // Play Sound On Mouseover
+            PlaySound("Audio/buttonHover.wav", "buttonHover", buttonClickVolume);
+
             // Change the image source to the highlighted version
             minimizeImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/minimize_button_highlight.png"));
         }
